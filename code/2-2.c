f9062036594ed3e50ad97b97c33ff7042f51fd22
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-03 10:32:54 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -5,7 +5,7 @@ void sort_strings(char** arr, int size) {
   char* temp;
   for (int i = 0; i < size - 1; i++) {
       for (int j = 0; j < size - 1 - i; j++) {
-        if (strcmp(arr[j], arr[j+1] > 0) {
+        if (strcmp(arr[j], arr[j+1]) > 0) {
             temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
