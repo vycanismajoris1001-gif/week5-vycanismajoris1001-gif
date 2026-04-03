0e6b2a6be998760b82977fe84b512a69608a91ca
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-03 10:02:10 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum_range(int* start, int* end) {
+  int count = end - start;
+  int sum = 0;
+  for (int i=?; i<?; i++) {
+    sum += start[i];
+  }
+  return sum;
+}
+
+int main() {
+  int data[] = {10,20,30,40,50,60,70};
+
+  int result = sum_range(&data[1], &data[4]);
+
+  printf("%d\n", result);
+  return 0;
+}
