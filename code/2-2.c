044437657ede4fc11c300c2291cfe1300d534d4b
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-03 10:31:53 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+void sort_strings(char** arr, int size) {
+  char* temp;
+  for (int i = 0; i < size - 1; i++) {
+      for (int j = 0; j < size - 1 - i; j++) {
+        if (strcmp(arr[j], arr[j+1] > 0) {
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+
+      }
+  }
+}
+
+int main() {
+  char* words[] = {"apple", "ant", "banana", "cherry"};
+  int size = 0;
+
+  sort_strings(word, size);
+
+  for (int i=0; i < size; i++) {
+     printf("%s\n", words[i]);
+  }
+return 0;
+}
