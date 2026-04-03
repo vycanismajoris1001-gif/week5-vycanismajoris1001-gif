7e6645c789d0e0de0ea897c72c2f94593a36b207
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-03 10:09:19 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_reverse(int* arr, int size) {
+  for (int i=arr[size-1];i>0;i--) {
+    printf(arr[i]);
+  }
+}
+
+int main() {
+  int nums[] = {1,2,3,4,5};
+
+  print_reverse(nums, 5);
+  printf("\n");
+
+  return 0;
+}
