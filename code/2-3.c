c0fe52ac5cee461a21400187f226e799573ef29b
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-03 09:35:55 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -2,22 +2,22 @@
 
 int main() {
   int a = 10, b = 20, c = 30, d = 40;
-  int* arr[] = {&a, &b, &c, &d};
-  int** pp = arr;
+  int *arr[] = {&a, &b, &c, &d};
+  int **pp = arr;
 
   
-  int** base = arr;
+  int **base = arr;
 
-  printf("%ld\n", (char* )pp - (char* )base);
-
-  pp = pp + 1;
-  printf("%ld\n", (char* )pp - (char* )base);
+  printf("%ld\n", (char *)pp - (char *)base);
 
   pp = pp + 1;
-  printf("%ld\n", (char* )pp - (char* )base);
+  printf("%ld\n", (char *)pp - (char *)base);
 
   pp = pp + 1;
-  printf("%ld\n", (char* )pp - (char* )base);
+  printf("%ld\n", (char *)pp - (char *)base);
+
+  pp = pp + 1;
+  printf("%ld\n", (char *)pp - (char *)base);
 
  return 0;
 }
