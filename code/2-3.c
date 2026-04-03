b5e8caf57b3125870e75197aaeb16311a24517eb
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-03 09:15:39 +0900

    Update 2-3.c

---
+++ code/2-3.c
@@ -0,0 +1,22 @@
+#include <stido.h>
+
+int main() {
+  int a = 10, b = 20, c = 30, d = 40;
+  int* arr[] = {&a, &b, &c, &d};
+  int** pp = arr;
+
+  int** base = arr;
+
+  printf("%ld\n", (char* )pp - (char* )base);
+
+  pp = pp + 1;
+  printf("%ld\n", (char* )pp - (char* )base);
+
+  pp = pp + 1;
+  printf("%ld\n", (char* )pp - (char* )base);
+
+  pp = pp + 1;
+  printf("%ld\n", (char* )pp - (char* )base);
+
+ return 0;
+}
